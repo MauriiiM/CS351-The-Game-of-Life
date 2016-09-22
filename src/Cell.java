a2c14702cb098cec0e5ce637efa206791848db6a
Mauricio Monsivais <mmonsivais@unm.edu>
2016-09-22 13:46:34 -0600

    init

--- src/Cell.java
+++ src/Cell.java
@@ -1,10 +1,15 @@
+import javafx.scene.Group;
+import javafx.scene.shape.Box;
+
 /**
  * Created by mmons on 9/22/2016.
  */
-public class Cell
+public class Cell extends Group
 {
+  Box cell;
   Cell()
   {
-
+    cell = new Box();
   }
+
 }
